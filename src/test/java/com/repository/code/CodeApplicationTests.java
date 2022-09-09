e43b8f301f703536af8979b98ccbbad544ae58a0
PersesTitan <torampersesTitan@gmail.com>
2022-09-09 18:02:00 +0900

    create project

---
+++ src/test/java/com/repository/code/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repository.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
